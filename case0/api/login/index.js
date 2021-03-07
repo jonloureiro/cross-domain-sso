62e2a18de4d1dd32670bd2627fe7acf02ab7c3c1
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2021-03-07 09:29:44 -0400

    add mongo connection

--- case0/api/login/index.js
+++ case0/api/login/index.js
@@ -1,8 +1,11 @@
 'use strict'
 
 const httpResponse = require('../../lib/httpResponse')
+const getMongoClient = require('../../lib/mongoClient')
 
-exports.handler = async function (event) {
+exports.handler = async function (event, context) {
+  context.callbackWaitsForEmptyEventLoop = false
+
   if (event.httpMethod !== 'POST') {
     return httpResponse.METHOD_NOT_ALLOWED
   }
@@ -17,8 +20,21 @@ exports.handler = async function (event) {
     return httpResponse.FORBIDDEN
   }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' })
+  try {
+    const mongoClient = await getMongoClient()
+    const result = await mongoClient
+      .db('cross-domain-sso')
+      .collection('users')
+      .findOne({ username: body.username })
+
+    if (!result) {
+      return httpResponse.FORBIDDEN
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Success' })
+    }
+  } catch (error) {
+    return httpResponse.SERVICE_UNAVAILABLE
   }
 }
---
+++ case0/lib/mongoClient.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const { MongoClient } = require('mongodb')
+
+let _mongoClient
+
+module.exports = async function getMongoClient () {
+  if (_mongoClient !== undefined) {
+    return _mongoClient
+  }
+
+  _mongoClient = await MongoClient.connect(process.env.MONGO_URI)
+  return _mongoClient
+}

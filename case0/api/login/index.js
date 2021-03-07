b8e3554e0592e740f0680e0cf7d845b16d10d861
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2021-03-07 05:08:31 -0400

    add lib httpResponse

--- case0/api/login/index.js
+++ case0/api/login/index.js
@@ -1,4 +1,10 @@
+const httpResponse = require('../../lib/httpResponse')
+
 exports.handler = async function (event) {
+  if (event.httpMethod !== "POST") {
+    return httpResponse.METHOD_NOT_ALLOWED;
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
---
+++ case0/lib/httpResponse.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+exports.FORBIDDEN = {
+  statusCode: 403,
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ message: http.STATUS_CODES[403] }),
+}
+
+exports.METHOD_NOT_ALLOWED = {
+  statusCode: 405,
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ message: http.STATUS_CODES[405] }),
+}

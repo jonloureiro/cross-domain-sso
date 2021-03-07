44c04a8e464fa4ffb50e8a4202910531b8051448
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2021-03-07 10:55:55 -0400

    remove Promise.resolve

--- case0/lib/__mocks__/findUserByUsername.js
+++ case0/lib/__mocks__/findUserByUsername.js
@@ -2,11 +2,11 @@
 
 module.exports = async function findUserByUsername (mongoClient, username) {
   if (username === 'example') {
-    return Promise.resolve({
+    return {
       _id: '6044e81557c2a280ffb3f0b7',
       username: 'example',
       password: '$2a$10$SSvJdlexJRSYI405YK9cfOVNAgNBYN4Y/VOVGX3fU1s1mvkB3UxrW'
-    })
+    }
   }
-  return Promise.resolve(null)
+  return null
 }

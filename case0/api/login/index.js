16905316f659d6787ab3ea7cd378a7b27e481077
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2021-03-07 09:32:17 -0400

    refactoring

--- case0/api/login/index.js
+++ case0/api/login/index.js
@@ -6,19 +6,13 @@ const getMongoClient = require('../../lib/mongoClient')
 exports.handler = async function (event, context) {
   context.callbackWaitsForEmptyEventLoop = false
 
-  if (event.httpMethod !== 'POST') {
-    return httpResponse.METHOD_NOT_ALLOWED
-  }
+  if (event.httpMethod !== 'POST') return httpResponse.METHOD_NOT_ALLOWED
 
-  if (!event.body) {
-    return httpResponse.FORBIDDEN
-  }
+  if (!event.body) return httpResponse.FORBIDDEN
 
   const body = JSON.parse(event.body)
 
-  if (!body.username || !body.password) {
-    return httpResponse.FORBIDDEN
-  }
+  if (!body.username || !body.password) return httpResponse.FORBIDDEN
 
   try {
     const mongoClient = await getMongoClient()
@@ -27,9 +21,8 @@ exports.handler = async function (event, context) {
       .collection('users')
       .findOne({ username: body.username })
 
-    if (!result) {
-      return httpResponse.FORBIDDEN
-    }
+    if (!result) return httpResponse.FORBIDDEN
+
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Success' })

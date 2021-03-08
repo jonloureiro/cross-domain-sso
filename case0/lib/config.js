bc81bb87f354234f01b4c389d3859f8837ae9f71
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2021-03-08 10:26:02 -0400

    add jwt secret

--- case0/lib/config.js
+++ case0/lib/config.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const config = {
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017'
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017',
+  SECRET: process.env.SECRET || 's3cr3t'
 }
 
 Object.freeze(config)

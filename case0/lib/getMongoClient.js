8b839e8dec9a71a9f2e6babef2367c70b63d1721
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2021-03-07 22:56:24 -0400

    add check if mongClient isConnected

--- case0/lib/getMongoClient.js
+++ case0/lib/getMongoClient.js
@@ -7,7 +7,7 @@ const config = require('./config')
 let _mongoClient
 
 module.exports = async function getMongoClient (mongoClientOptions) {
-  if (_mongoClient) return _mongoClient
+  if (_mongoClient && _mongoClient.isConnected()) return _mongoClient
   _mongoClient = await MongoClient.connect(config.MONGO_URI, { ...mongoClientOptions, useUnifiedTopology: true })
   return _mongoClient
 }

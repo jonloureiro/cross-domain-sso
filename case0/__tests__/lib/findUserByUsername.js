bcd9e28f2e98b99c1985756dcd5593167ce5b15b
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2021-03-09 03:47:05 -0400

    refactoring mockUser

--- case0/__tests__/lib/findUserByUsername.js
+++ case0/__tests__/lib/findUserByUsername.js
@@ -16,24 +16,26 @@ afterAll(async () => {
 })
 
 test('Find an existing user', async () => {
-  const username = crypto.randomBytes(20).toString('hex')
-  const mockUser = { _id: 'test', username, password: '2' }
-  await mongoClient
+  const mockUser = {
+    username: crypto.randomBytes(8).toString('hex'),
+    password: crypto.randomBytes(8).toString('hex')
+  }
+  const { insertedId } = await mongoClient
     .db('cross-domain-sso')
     .collection('users')
     .insertOne(mockUser)
 
-  const result = await findUserByUsername(username)
+  const result = await findUserByUsername(mockUser.username)
   expect(result).toEqual(mockUser)
 
   await mongoClient
     .db('cross-domain-sso')
     .collection('users')
-    .deleteOne(mockUser)
+    .deleteOne({ _id: insertedId })
 })
 
 test('Do not find an existing user', async () => {
-  const username = crypto.randomBytes(20).toString('hex')
+  const username = crypto.randomBytes(8).toString('hex')
   const result = await findUserByUsername(username)
   expect(result).toBeFalsy()
 })

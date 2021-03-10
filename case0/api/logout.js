1a5940d268b69cb0d8e3ba1ce4366f4cdbe5c8e7
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2021-03-10 03:03:14 -0400

    fix logout

--- case0/api/logout.js
+++ case0/api/logout.js
@@ -1,29 +1,34 @@
 'use strict'
 
-// TODO: tests
-
 const httpResponse = require('../lib/httpResponse')
 const invalidateRefreshToken = require('../lib/invalidateRefreshToken')
+
+const RESPONSE_DEFAULT = {
+  statusCode: 200,
+  headers: {
+    'Content-Type': 'application/json',
+    'Set-Cookie': 'token=; Max-Age=1; Secure; HttpOnly;'
+  }
+}
 
 exports.handler = async function (event, context) {
   if (context) context.callbackWaitsForEmptyEventLoop = false
 
   if (event.httpMethod !== 'POST') return httpResponse.METHOD_NOT_ALLOWED
 
-  if (event.cookie) {
-    try {
-      await invalidateRefreshToken()
-    } catch (error) {
-      return {
-        statusCode: 200,
-        headers: { 'Set-Cookie': 'token=; Max-Age=1; Secure; HttpOnly;' },
-        body: JSON.stringify({ error }) // TODO: debug
-      }
-    }
-  }
+  if (!event.headers.cookie) return RESPONSE_DEFAULT
 
-  return {
-    statusCode: 200,
-    headers: { 'Set-Cookie': 'token=; Max-Age=1; Secure; HttpOnly;' }
+  const [, accessToken] = event.headers.cookie.split('=')
+
+  if (!accessToken) return RESPONSE_DEFAULT
+
+  try {
+    await invalidateRefreshToken(accessToken)
+    return RESPONSE_DEFAULT
+  } catch (error) {
+    return {
+      ...RESPONSE_DEFAULT,
+      body: JSON.stringify({ error: error.message })
+    }
   }
 }

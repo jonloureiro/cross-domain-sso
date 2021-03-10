af19be304ace0604e7c482d4762de98ccdafcb1c
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2021-03-10 10:56:44 -0400

    fix helper

--- case0/api/helper.js
+++ case0/api/helper.js
@@ -1,12 +1,12 @@
 const parser = require('ua-parser-js')
 
 exports.handler = async function (event) {
-  const { ua } = parser(event.headers['user-agent'])
+  const client = parser(event.headers['user-agent'])
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      ua,
+      client,
       event
     })
   }
